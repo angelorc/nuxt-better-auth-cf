e05944104ebc519a8ebbc9ad7c816faac40afa34
angelorc <angelo@rcstart.it>
2025-04-18 14:21:40 +0200

    feat: add postgres dependency and update db utility for better compatibility

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,11 +1,19 @@
 import { drizzle as drizzle_neon_http } from 'drizzle-orm/neon-http'
+import { drizzle as drizzle_pg } from 'drizzle-orm/postgres-js'
 import { neon } from "@neondatabase/serverless";
+import postgres from 'postgres'
 
-type NeonHttp = ReturnType<typeof drizzle_neon_http>
+type NeonHttp = ReturnType<typeof drizzle_neon_http> | ReturnType<typeof drizzle_pg>
 
 let _db: NeonHttp
 export function db(): NeonHttp {
   if (_db) {
+    return _db
+  }
+
+  if (import.meta.dev) {
+    const client = postgres(process.env.DATABASE_URL as string);
+    _db = drizzle_pg({ client });
     return _db
   }
 
